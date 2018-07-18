2c8004f16c7e78d70bc1121e91033e96c2e00092
Guy G <guy.gavergun@gmail.com>
2018-07-17 20:38:32 -0400

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,20 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+let password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+let hashedPassword = "$2a$10$t0nk1vffSYWsNbZiyZi6T.HYHrHsQleu9mXdTTujNu6E27LHI0WVi";
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+  console.log(result);
+});
 
 let data = {
   id: 10  
@@ -11,27 +26,3 @@ console.log(token);
 let decoded = jwt.verify(token, '123abc');
 
 console.log(decoded);
-
-// let message = 'Some string n';
-//
-// let hash = SHA256(message).toString();
-//
-// console.log(`Message: ${message}`);
-// console.log(`Hash: ${hash}`);
-//
-// let data = {
-//     id: 4,
-// };
-//
-// let token = {
-//     data,
-//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// };
-//
-// let resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-//
-// if ( resultHash === token.hash ) {
-//     console.log('Data was not changed');
-// } else {
-//     console.log('Data was changed');
-// }
--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const _ = require('lodash');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 let UserSchema = new mongoose.Schema({
     email: {
@@ -32,40 +33,55 @@ let UserSchema = new mongoose.Schema({
     }]
 });
 
-UserSchema.methods.toJSON = function() {
+UserSchema.methods.toJSON = function () {
     let user = this;
     let userObject = user.toObject();
     return _.pick(userObject, ['_id', 'email'])
 };
 
-UserSchema.methods.generateAuthToken = function() {
-  let user = this;
-  let access = 'auth';
-  let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
-  
-  user.tokens = user.tokens.concat([{access, token}]);
-  
-  return user.save().then(() => {
-      return token;
-  })
+UserSchema.methods.generateAuthToken = function () {
+    let user = this;
+    let access = 'auth';
+    let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+        return token;
+    })
 };
 
-UserSchema.statics.findByToken = function(token) {
+UserSchema.statics.findByToken = function (token) {
     let User = this;
     let decoded;
-    
+
     try {
         decoded = jwt.verify(token, 'abc123');
-    } catch(e) {
+    } catch (e) {
         return Promise.reject();
     }
-    
+
     return User.findOne({
         '_id': decoded._id,
         'tokens.token': token,
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.pre('save', function (next) {
+    let user = this;
+
+    if ( user.isModified('password') ) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 let User = mongoose.model('User', UserSchema);
 
